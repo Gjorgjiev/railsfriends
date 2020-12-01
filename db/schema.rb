b58e2d4763d9aa7f503d0bea109e7203e96c1cd3
Darko Gjorgjiev <johndoe@example.com>
2020-12-01 16:51:11 +0100

    initial commit

---
+++ db/schema.rb
@@ -0,0 +1,28 @@
+
+ActiveRecord::Schema.define(version: 2020_12_01_115547) do
+
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "twitter"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "model_id"
+    t.index ["model_id"], name: "index_friends_on_model_id"
+  end
+
+  create_table "models", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_models_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+  end
+
+end
