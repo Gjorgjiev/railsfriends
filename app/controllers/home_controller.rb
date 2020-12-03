103e1d5c44df454f6668314e5c708fdfc888765f
Darko Gjorgjiev <dare@gmail.com>
2020-12-03 14:26:17 +0100

    updated About Us page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,13 @@ class HomeController < ApplicationController
   end
 
   def about
-  	@about_me = "My name is Darko"
-  	@answer = 2+2
+  	@about_us = "MyFriendsApp is a web-site where people can add friends, their phone numbers, emails, links to their social
+  	 network web-sites. 
+  	 The idea is same just like we have our contacts list in our mobile phones, just with more options and easier to 
+  	 save and navigate through them." +
+
+  	  " You dont need to post pictures of yourself, if you want you can add an avatar as a profile 
+  	 picture, but that is up to you. "
+  	
   end
 end
